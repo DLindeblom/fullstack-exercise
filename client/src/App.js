126437b8b9508166dfee6e1aac53c70678ff6668
DLindeblom <drdlindeblom@gmail.com>
2022-10-11 14:32:51 -0400

    add basic styling to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
+import { Container, Stack, Button, Table } from 'react-bootstrap';
 
 function App() {
+
+  useEffect(() => {
+    axios.get('/employees')
+  })
   return (
-    <div>
-      Plexxis Test
+    <div className="vh-100" style={{ background: "linear-gradient(#e66465, #9198e5)" }}>
+      <Container >
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto mt-5">Plexxis Employees</h1>
+          <Button>Add Employee</Button>
+        </Stack>
+        <Stack>
+          <Table bordered>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Code</th>
+                <th>Profession</th>
+                <th>City</th>
+                <th>Branch</th>
+                <th>Abacus</th>
+                <th>Assigned</th>
+                <th>Options</th>
+              </tr>
+            </thead>
+          </Table>
+        </Stack>
+      </Container>
+
     </div>
   );
 }
