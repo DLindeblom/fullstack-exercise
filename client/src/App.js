d9f96bda4f92b03ddead748316ebc7b5a5107361
DLindeblom <drdlindeblom@gmail.com>
2022-10-11 16:09:42 -0400

    minor formatting change

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 import { Container, Stack, Button, Table } from 'react-bootstrap';
 import axios from 'axios';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   const [employees, setEmployees] = useState([])
 
-  useEffect(() => {
+  useEffect(() => { //get employee array and set employees
     axios.get('/employees')
          .then((res) => {
           setEmployees(res.data)
@@ -46,7 +47,7 @@ function App() {
       <Container >
         <Stack direction="horizontal" gap="2" className="mb-4">
           <h1 className="me-auto mt-5">Plexxis Employees</h1>
-          <Button>Add Employee</Button>
+          <Button className="mt-5">Add Employee</Button>
         </Stack>
         <Stack>
           <Table bordered hover striped>
