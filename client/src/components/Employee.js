74a0f793ada1c55b7df01f8de57fbc4c1df6d4cd
DLindeblom <drdlindeblom@gmail.com>
2022-10-12 13:29:23 -0400

    add Delete employee modal information and stateto this component

--- client/src/components/Employee.js
+++ client/src/components/Employee.js
@@ -1,31 +1,54 @@
 import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { DeleteEmployeeModal } from './DeleteEmployeeModal';
 
 export const Employee = ({
-  id, 
-  name, 
+  id,
+  name,
   code,
   profession,
   color,
   city,
   branch,
-  assigned
+  assigned,
 }) => {
 
+  const [showDeleteEmployeeModal, setShowDeleteEmployeeModal] = useState(false);
+  const [deleteEmployeeModalId, setDeleteEmployeeModalId] = useState();
+
+  const openDeleteEmployeeModal = (id) => {
+    setShowDeleteEmployeeModal(true)
+    setDeleteEmployeeModalId(id)
+  }
 
   return (
-    <tr key={id}>
-    <td>{id}</td>
-    <td>{name}</td>
-    <td>{code}</td>
-    <td>{profession}</td>
-    <td style={{"backgroundColor": color}}></td>
-    <td>{city}</td>
-    <td>{branch}</td>
-    <td>{assigned}</td>
-    <td className="d-flex justify-content-between">
-      <Button className="btn btn-danger btn-sm">Delete</Button>
-      <Button className="btn btn-secondary btn-sm">Edit</Button>
-    </td>
-  </tr>
-  )
-}
+    <>
+      <tr key={id}>
+        <td>{id}</td>
+        <td>{name}</td>
+        <td>{code}</td>
+        <td>{profession}</td>
+        <td style={{ "backgroundColor": color }}></td>
+        <td>{city}</td>
+        <td>{branch}</td>
+        <td>{assigned}</td>
+        <td className="d-flex justify-content-between">
+          <Button
+            className="btn btn-danger btn-sm"
+            onClick={() => openDeleteEmployeeModal(id)}
+          >
+            Delete
+          </Button>
+          <Button className="btn btn-secondary btn-sm">Edit</Button>
+        </td>
+      </tr>
+
+      <DeleteEmployeeModal
+        show={showDeleteEmployeeModal}
+        handleClose={() => setShowDeleteEmployeeModal(false)}
+        employeeId={id}
+        employeeName={name}
+      />
+    </>
+  );
+};
