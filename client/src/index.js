12dccd7299cd144d027eb6983e970ed246587fc6
DLindeblom <drdlindeblom@gmail.com>
2022-10-12 10:11:51 -0400

    comment out the use of context

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// import { EmployeesProvider } from './context/EmployeeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <EmployeesProvider> */}
+      <App />
+    {/* </EmployeesProvider> */}
   </React.StrictMode>
 );
 
