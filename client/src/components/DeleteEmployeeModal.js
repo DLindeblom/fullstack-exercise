12af1b5c3db7624ea74dfa106097605dd7e692ff
DLindeblom <drdlindeblom@gmail.com>
2022-10-12 13:29:49 -0400

    create delete employee modal

---
+++ client/src/components/DeleteEmployeeModal.js
@@ -0,0 +1,31 @@
+import { Modal, Button, Stack } from 'react-bootstrap';
+import { useEmployees } from '../hooks/useEmployees';
+
+export const DeleteEmployeeModal = ({ show, handleClose, employeeId, employeeName }) => {
+
+  const { deleteEmployee } = useEmployees();
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          Delete Employee
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className='text-center'>
+        <h5>
+          Are you sure you want to delete {employeeName}?
+        </h5>
+      </Modal.Body>
+      <Stack direction='horizontal' gap='2' className='d-flex justify-content-around mt-4 mb-3'>
+        <Button 
+          className="btn btn-danger"
+          onClick={() => deleteEmployee(employeeId)}
+        >
+          Delete
+        </Button>
+        <Button className="btn btn-secondary" onClick={handleClose}>Cancel</Button>
+      </Stack>
+    </Modal>
+  );
+};
