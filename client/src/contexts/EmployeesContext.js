4fa6a0e8c1b57915eb4a92af191c2c83b9907293
DLindeblom <drdlindeblom@gmail.com>
2022-10-12 15:56:46 -0400

    write update employee function to handle changes to employee information

--- client/src/contexts/EmployeesContext.js
+++ client/src/contexts/EmployeesContext.js
@@ -28,12 +28,10 @@ export const EmployeesProvider = ({ children }) => {
         setEmployees(prevEmployees => {
           return [...prevEmployees, { name, code, profession, color, city, branch, assigned }]
         })
-        // axios.get('/employees')
-        //   .then((res) => {
-        //     setEmployees(res.data);
-        //   });
       });
   };
+
+  //the issue with this method of updating state after adding and employee is that I don't have the ID of the user from the database so if the get request isnt made, I cant delete or edit this information until the ID eventually comes from the database.  I could make a get request inside of my .then() and reset the employee state but the could be expensive if employee table is large!
 
   function deleteEmployee(id) { //delete an employee
     axios.delete(`/employees/delete/${id}`)
@@ -42,14 +40,31 @@ export const EmployeesProvider = ({ children }) => {
           return prevEmployees.filter(employee => employee.id !== id)
         })
       });
+  }
 
+  function updateEmployee({ id, name, code, profession, color, city, branch, assigned }) {
+    console.log(color)
+    axios.put(`/employees/update/${id}`, {id, name, code, profession, color, city, branch, assigned})
+    .then((res) => {
+      if(res.status === 200) {
+        axios.get('/employees')
+        .then((res) => {
+          console.log(res.data)
+          setEmployees(res.data);
+        });
+        // setEmployees(prevEmployees => {
+        //   return [...prevEmployees, { id, name, code, profession, color, city, branch, assigned }]
+        // })
+      }
+    })
   }
 
   return (
     <EmployeesContext.Provider value={{
       employees, 
       addEmployee,
-      deleteEmployee
+      deleteEmployee,
+      updateEmployee
     }}>{children}</EmployeesContext.Provider>
   )
 
