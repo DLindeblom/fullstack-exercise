1d04a1c5bd6efca71364957c4ef190d2825845d0
DLindeblom <drdlindeblom@gmail.com>
2022-10-13 09:23:14 -0400

    remove a Stack element

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Container, Stack, Button, Table } from 'react-bootstrap';
+import { Container, Stack, Button } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AddEmployeeModal } from './components/AddEmployeeModal';
@@ -16,9 +16,7 @@ function App() {
           <h1 className="me-auto mt-5">Plexxis Employees</h1>
           <Button className="mt-5" onClick={() => setShowAddEmployeeModal(true)}>Add Employee</Button>
         </Stack>
-        <Stack>
-          <EmployeeTable/>
-        </Stack>
+        <EmployeeTable/>
       </Container>
 
       <AddEmployeeModal
