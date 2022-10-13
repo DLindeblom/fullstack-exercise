6640541b3593345794713152d5339a1ce38b38c3
DLindeblom <drdlindeblom@gmail.com>
2022-10-13 09:20:56 -0400

    separate table markup and employee component logic and put it in a separate component

--- client/src/App.js
+++ client/src/App.js
@@ -9,35 +9,6 @@ function App() {
 
   const [showAddEmployeeModal, setShowAddEmployeeModal] = useState(false);
 
-  // const { employees } = useEmployees();
-
-  // //create an Employee component by looping through the employee array
-
-  // const oneEmployee = employees.map(employee => {
-
-  //   //convert boolean from data, to string to be displayed in table
-  //   let assigned = ""
-  //   if(employee.assigned) {
-  //     assigned = "True"
-  //   } else { 
-  //     assigned = "False"
-  //   }
-
-  //   return (
-  //     <Employee
-  //       key={employee.id}
-  //       id={employee.id}
-  //       name={employee.name} 
-  //       code={employee.code}
-  //       profession={employee.profession}
-  //       color={employee.color}
-  //       city={employee.city}
-  //       branch={employee.branch}
-  //       assigned={assigned}
-  //     />
-  //   )
-  // })
-
   return (
     <div className="vh-100" style={{"background": "linear-gradient(to right, #D7E1EC, #FFFFFF"}}>
       <Container >
@@ -46,23 +17,6 @@ function App() {
           <Button className="mt-5" onClick={() => setShowAddEmployeeModal(true)}>Add Employee</Button>
         </Stack>
         <Stack>
-          {/* <Table bordered hover striped>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Code</th>
-                <th>Profession</th>
-                <th>Color</th>
-                <th>City</th>
-                <th>Branch</th>
-                <th>Assigned</th>
-                <th>Options</th>
-              </tr>
-            </thead>
-            <tbody>
-              {oneEmployee}
-            </tbody>
-          </Table> */}
           <EmployeeTable/>
         </Stack>
       </Container>
