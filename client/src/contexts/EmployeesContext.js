b1c61ea63600152a4758dc58edaf068a9eb25ac8
DLindeblom <drdlindeblom@gmail.com>
2022-10-16 19:22:31 -0400

    add state for alert variant and message; set message and variant inside successful axios request and set message and variant inside catch block if promise did not resolve

--- client/src/contexts/EmployeesContext.js
+++ client/src/contexts/EmployeesContext.js
@@ -13,6 +13,9 @@ export function useEmployees() {
 export const EmployeesProvider = ({ children }) => {
 
   const [employees, setEmployees] = useState([]);
+  const [variant, setVariant] = useState("");
+  const [alertText, setAlertText] = useState("");
+
 
   useEffect(() => { //get employee array and set employees
     axios.get('/employees')
@@ -23,37 +26,60 @@ export const EmployeesProvider = ({ children }) => {
 
   function addEmployee({ name, code, profession, color, city, branch, assigned }) { //add an employee
     axios.post('/employees', { name, code, profession, color, city, branch, assigned })
-      .then(res => console.log(res))
-      .then(() => {
-        setEmployees(prevEmployees => {
-          return [...prevEmployees, { name, code, profession, color, city, branch, assigned }];
-        });
-      });
+      .then((res) => {
+        console.log("response", res.status)
+        if(res.status === 200) {
+          setVariant("success")
+          setAlertText("Employee successfully added!")
+          setEmployees(prevEmployees => {
+            return [...prevEmployees, { name, code, profession, color, city, branch, assigned }];
+          });
+        } 
+      })
+      .catch(() => {
+        setVariant("danger")
+        setAlertText("Oops, something went wrong!")
+      })
   };
 
   //the issue with this method of updating state after adding and employee is that I don't have the ID of the user from the database so if the get request isnt made, I cant delete or edit this information until the ID eventually comes from the database.  I could make a get request inside of my .then() and reset the employee state but the could be expensive if employee table is large!
 
   function deleteEmployee(id) { //delete an employee
     axios.delete(`/employees/delete/${id}`)
-      .then(() => {
-        //filter out the employee from the employees array whos id matches the id of the employee being deleted from the database
-        setEmployees(prevEmployees => {
-          return prevEmployees.filter(employee => employee.id !== id);
-        });
-      });
+      .then((res) => {
+        console.log(res.status)
+        if (res.status === 200 ) {
+          setVariant("success")
+          setAlertText("Employee Successfully Deleted")
+          //filter out the employee from the employees array whos id matches the id of the employee being deleted from the database
+          setEmployees(prevEmployees => {
+            return prevEmployees.filter(employee => employee.id !== id);
+          });
+        }
+      })
+      .catch(() => {
+        setVariant("danger")
+        setAlertText("Oops, Something went wrong!")
+      })
   }
 
   function updateEmployee({ id, name, code, profession, color, city, branch, assigned }) {
-    console.log(color);
     axios.put(`/employees/update/${id}`, { id, name, code, profession, color, city, branch, assigned })
       .then((res) => {
+        console.log(res.status)
         if (res.status === 200) {
+          setVariant("success")
+          setAlertText("Employee Successfully Updated!")
           // filter out the existing entry with the old data, and spread the rest of the array and add the edited employee information to the employee array
           setEmployees(prevEmployees => {
             return [...prevEmployees.filter(employee => employee.id !== id), { id, name, code, profession, color, city, branch, assigned }];
           });
-        }
-      });
+        } 
+      })
+      .catch(() => {
+        setVariant("danger")
+        setAlertText("Oops, Something went wrong!")
+      })
   }
 
   //unintended side effect of this approach is the employee being edited is now placed at the end of the array.  Not sure if refetching the data to all the employee to remain in the same location on the table would be better?
@@ -62,6 +88,9 @@ export const EmployeesProvider = ({ children }) => {
   return (
     <EmployeesContext.Provider value={{
       employees,
+      variant,
+      alertText,
+      setAlertText,
       addEmployee,
       deleteEmployee,
       updateEmployee
